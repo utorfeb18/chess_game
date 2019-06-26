09fe7f610196c60c845c9f03e2793bf061826747
Fil Gambatesa <fil.gambatesa@gmail.com>
2019-06-26 13:24:01 -0400

    Add root route and generic hello world page.  Removes FactoryBot code

---
+++ app/helpers/chess_board_helper.rb
@@ -0,0 +1,2 @@
+module ChessBoardHelper
+end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,16 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  describe "test test" do
-    it "should have a home page" do
-      expect("pizza").to eq "pizza"
-    end
-  end
-
-  describe "landing-page#index action" do
-    it "should successfully show the page" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
 end
